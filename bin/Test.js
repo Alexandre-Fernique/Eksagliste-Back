ede3c0ddc0b2c9e322507a7c2b1cde9151a35133
Alexandre <alexandre.fernique@etu.umontpellier.fr>
2022-03-17 23:35:44 +0100

    Add Entry test

--- bin/Test.js
+++ bin/Test.js
@@ -1,10 +1,18 @@
 
-function email(email =""){
-    if(email.length <= 200 || email.endsWith("@etu.umontpellier.fr")){
-        return true
-    }
-    return false
+function email(email){
+    return (email.length <= 200 || email.endsWith("@etu.umontpellier.fr"));
+
+
+}
+function text(text){
+    return text.length <= 500;
+
+
+}
+function password(password){
+    return password.length >= 8;
+
 
 }
 
-module.exports={email}
+module.exports={email,password,text}
